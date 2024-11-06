e10f40523643f3e4c8d2a169e5b1a4af143759c5
Juli Taras <30576222+julitaras@users.noreply.github.com>
2024-11-06 15:56:22 -0300

    refactor and validation (#15)

--- src/reads/pages/BookProfilePage.jsx
+++ src/reads/pages/BookProfilePage.jsx
@@ -264,174 +264,195 @@ export const BookProfile = () => {
   const fullName = `${user?.firstName} ${user?.lastName}`;
   const isAuthor = user?.role === "escritor" && fullName === author;
 
-  return (
-    <div className='min-h-screen bg-gradient-to-br from-gray-900 to-blue-900 text-white py-12'>
-      <div className='container mx-auto px-4'>
-        <div className='grid md:grid-cols-3 gap-8'>
-          <div className='md:col-span-1'>
-            <img
-              src={cover_image_url}
-              alt={`Portada de ${title}`}
-              className='w-full h-auto rounded-lg shadow-lg border-2 border-blue-500'
-            />
-          </div>
-          <div className='md:col-span-2 space-y-6'>
-            <h1 className='text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-600'>
-              {title}
-            </h1>
-            <p className='text-xl text-blue-300'>por {author}</p>
-            <div className='flex items-center space-x-4 flex-wrap'>
-              <Badge variant='secondary' className='bg-blue-600 text-white'>
-                {genre}
-              </Badge>
-              <div className='flex items-center text-blue-300'>
-                <Clock className='w-4 h-4 mr-1' />
-                <span>{new Date(published_date).getFullYear()}</span>
-              </div>
-              <div className='flex items-center'>
-                <Star className='w-4 h-4 mr-1 text-yellow-400' />
-                <span className='text-blue-300'>{rating.toFixed(1)}</span>
-              </div>
-              <div className='flex items-center text-blue-300'>
-                <BookOpen className='w-4 h-4 mr-1' />
-                <span>{page_count} páginas</span>
-              </div>
-            </div>
-            <div className='bg-gray-800 p-6 rounded-lg border border-blue-500 shadow-lg'>
-              <h2 className='text-2xl font-semibold mb-2 text-blue-400 text-left'>
-                Sinopsis
-              </h2>
-              <p className='text-blue-200'>{description}</p>
-            </div>
-            <div className='mt-6 flex items-center space-x-4'>
-              <button
-                onClick={handleBuyBook}
-                className='mt-6 px-4 py-2 bg-purple-600 text-white rounded-lg shadow-md hover:bg-purple-700 transition-colors duration-300 flex items-center'
-              >
-                <CircleDollarSign className='w-4 h-4 mr-1' />
-                Comprar libro
-              </button>
-              {isAuthor && (
-                <>
-                  <button
-                    className='mt-6 px-4 py-2 bg-purple-600 text-white rounded-lg shadow-md hover:bg-purple-700 transition-colors duration-300 flex items-center'
-                    onClick={handleUpdateBook}
-                  >
-                    <Edit className='w-4 h-4 mr-1' />
-                    Modificar Libro
-                  </button>
-                  <button
-                    className='mt-6 px-4 py-2 text-white rounded-lg shadow-md bg-red-600 hover:bg-red-700 transition-colors duration-300 flex items-center'
-                    onClick={handleDeleteBook}
-                  >
-                    <Trash2 className='w-4 h-4 mr-1' />
-                    Eliminar Libro
-                  </button>
-                </>
-              )}
+
+  const Review = ({review}) => (
+    <Card
+      key={review.id}
+      className='bg-gray-800 border border-blue-500 rounded-lg shadow-lg'
+    >
+      <CardContent className='p-6 bg-gray-800 p-6 rounded-lg border border-blue-500 shadow-lg'>
+        <div className='flex items-center space-x-4 mb-4'>
+          <Avatar className='border-2 border-blue-500'>
+            <AvatarImage src={review.avatar} alt={review.username} />
+            <AvatarFallback className='bg-blue-600 text-white'>
+              <User />
+            </AvatarFallback>
+          </Avatar>
+          <div>
+            <Link
+              to={(isSameUser(review, user)) ? `/profile` : `/users/${review.username}`}
+              className='text-white text-xl font-semibold mr-4 text-decoration-none'
+            >
+              <p className='font-semibold text-blue-300'>
+                {review.username}
+              </p>{" "}
+            </Link>
+            <div className='flex items-center'>
+              {[...Array(5)].map((_, i) => (
+                <Star
+                  key={i}
+                  className={`w-4 h-4 ${
+                    i < review.rating
+                      ? "text-yellow-400"
+                      : "text-gray-500"
+                  }`}
+                />
+              ))}
             </div>
           </div>
         </div>
-        <div className='mt-12'>
-          <h2 className='text-2xl font-semibold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600'>
-            Reseñas de lectores
-          </h2>
-          <div className='space-y-6'>
-            {reviews.map((review) => (
-              <Card
-                key={review.id}
-                className='bg-gray-800 border border-blue-500 rounded-lg shadow-lg'
-              >
-                <CardContent className='p-6 bg-gray-800 p-6 rounded-lg border border-blue-500 shadow-lg'>
-                  <div className='flex items-center space-x-4 mb-4'>
-                    <Avatar className='border-2 border-blue-500'>
-                      <AvatarImage src={review.avatar} alt={review.username} />
-                      <AvatarFallback className='bg-blue-600 text-white'>
-                        <User />
-                      </AvatarFallback>
-                    </Avatar>
-                    <div>
-                      <Link
-                        to={`/users/${review.username}`}
-                        className='text-white text-xl font-semibold mr-4 text-decoration-none'
-                      >
-                        <p className='font-semibold text-blue-300'>
-                          {review.username}
-                        </p>{" "}
-                      </Link>
-                      <div className='flex items-center'>
-                        {[...Array(5)].map((_, i) => (
-                          <Star
-                            key={i}
-                            className={`w-4 h-4 ${
-                              i < review.rating
-                                ? "text-yellow-400"
-                                : "text-gray-500"
-                            }`}
-                          />
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                  <div className='flex justify-between items-start'>
-                    <p className='text-blue-300 text-left flex-1 mr-4'>
-                      {review.content}
-                    </p>
-                    {/* Mostrar botones solo si el usuario es el autor de la reseña */}
-                    {user.username === review.username && (
-                      <div className='flex space-x-2'>
-                        <Button
-                          variant='ghost'
-                          onClick={() => handleEditReview(review)}
-                        >
-                          <Edit className='text-blue-500 hover:text-blue-700' />
-                        </Button>
-                        <Button
-                          variant='ghost'
-                          onClick={() => handleDeleteReview(review.id)}
-                        >
-                          <Trash2 className='text-red-500 hover:text-red-700' />
-                        </Button>
-                      </div>
-                    )}
-                  </div>
-                </CardContent>
-              </Card>
-            ))}
-          </div>
-
-          {user && (
-            <div className='mt-8'>
-              <h3 className='text-xl mb-4'>
-                {editingReviewId ? "Editar Reseña" : "Agregar Reseña"}
-              </h3>
-              <Textarea
-                placeholder='Escribe tu reseña aquí...'
-                value={newReview.content}
-                onChange={(e) =>
-                  setNewReview({ ...newReview, content: e.target.value })
-                }
-              />
-              <input
-                type='number'
-                max={5}
-                min={0}
-                value={newReview.rating}
-                onChange={(e) =>
-                  setNewReview({ ...newReview, rating: Number(e.target.value) })
-                }
-                className='w-full bg-gray-800 mt-2 p-2 text-blue-200 rounded'
-              />
+        <div className='flex justify-between items-start'>
+          <p className='text-blue-300 text-left flex-1 mr-4'>
+            {review.content}
+          </p>
+          {isSameUser(review, user) && (
+            <div className='flex space-x-2'>
               <Button
-                onClick={editingReviewId ? handleUpdateReview : handleAddReview}
-                className='mt-4'
+                variant='ghost'
+                onClick={() => handleEditReview(review)}
               >
-                {editingReviewId ? "Actualizar Reseña" : "Agregar Reseña"}
+                <Edit className='text-blue-500 hover:text-blue-700' />
+              </Button>
+              <Button
+                variant='ghost'
+                onClick={() => handleDeleteReview(review.id)}
+              >
+                <Trash2 className='text-red-500 hover:text-red-700' />
               </Button>
             </div>
+          )}
+        </div>
+      </CardContent>
+    </Card>
+  );
+
+  const BookReviews = ({ reviews }) => (
+    <div className='mt-12'>
+      <h2 className='text-2xl font-semibold mb-4 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600'>
+        Reseñas de lectores
+      </h2>
+      <div className='space-y-6'>
+        {reviews.map((review) => (<Review review={review} key={review.id} />))}
+      </div>
+
+      {user && (
+        <div className='mt-8'>
+          <h3 className='text-xl mb-4'>
+            {editingReviewId ? "Editar Reseña" : "Agregar Reseña"}
+          </h3>
+          <Textarea
+            placeholder='Escribe tu reseña aquí...'
+            value={newReview.content}
+            onChange={(e) =>
+              setNewReview({ ...newReview, content: e.target.value })
+            }
+          />
+          <input
+            type='number'
+            max={5}
+            min={0}
+            value={newReview.rating}
+            onChange={(e) =>
+              setNewReview({ ...newReview, rating: Number(e.target.value) })
+            }
+            className='w-full bg-gray-800 mt-2 p-2 text-blue-200 rounded'
+          />
+          <Button
+            onClick={editingReviewId ? handleUpdateReview : handleAddReview}
+            className='mt-4'
+          >
+            {editingReviewId ? "Actualizar Reseña" : "Agregar Reseña"}
+          </Button>
+        </div>
+      )}
+    </div>
+  );
+
+  const BookInformation = ({
+    title,
+    author,
+    published_date,
+    rating,
+    page_count,
+  }) => (
+    <div className='grid md:grid-cols-3 gap-8'>
+      <div className='md:col-span-1'>
+        <img
+          src={cover_image_url}
+          alt={`Portada de ${title}`}
+          className='w-full h-auto rounded-lg shadow-lg border-2 border-blue-500'
+        />
+      </div>
+      <div className='md:col-span-2 space-y-6'>
+        <h1 className='text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-600'>
+          {title}
+        </h1>
+        <p className='text-xl text-blue-300'>por {author}</p>
+        <div className='flex items-center space-x-4 flex-wrap'>
+          <Badge variant='secondary' className='bg-blue-600 text-white'>
+            {genre}
+          </Badge>
+          <div className='flex items-center text-blue-300'>
+            <Clock className='w-4 h-4 mr-1' />
+            <span>{new Date(published_date).getFullYear()}</span>
+          </div>
+          <div className='flex items-center'>
+            <Star className='w-4 h-4 mr-1 text-yellow-400' />
+            <span className='text-blue-300'>{rating.toFixed(1)}</span>
+          </div>
+          <div className='flex items-center text-blue-300'>
+            <BookOpen className='w-4 h-4 mr-1' />
+            <span>{page_count} páginas</span>
+          </div>
+        </div>
+        <div className='bg-gray-800 p-6 rounded-lg border border-blue-500 shadow-lg'>
+          <h2 className='text-2xl font-semibold mb-2 text-blue-400 text-left'>
+            Sinopsis
+          </h2>
+          <p className='text-blue-200'>{description}</p>
+        </div>
+        <div className='mt-6 flex items-center space-x-4'>
+          <button
+            onClick={handleBuyBook}
+            className='mt-6 px-4 py-2 bg-purple-600 text-white rounded-lg shadow-md hover:bg-purple-700 transition-colors duration-300 flex items-center'
+          >
+            <CircleDollarSign className='w-4 h-4 mr-1' />
+            Comprar libro
+          </button>
+          {isAuthor && (
+            <>
+              <button
+                className='mt-6 px-4 py-2 bg-purple-600 text-white rounded-lg shadow-md hover:bg-purple-700 transition-colors duration-300 flex items-center'
+                onClick={handleUpdateBook}
+              >
+                <Edit className='w-4 h-4 mr-1' />
+                Modificar Libro
+              </button>
+              <button
+                className='mt-6 px-4 py-2 text-white rounded-lg shadow-md bg-red-600 hover:bg-red-700 transition-colors duration-300 flex items-center'
+                onClick={handleDeleteBook}
+              >
+                <Trash2 className='w-4 h-4 mr-1' />
+                Eliminar Libro
+              </button>
+            </>
           )}
         </div>
       </div>
     </div>
   );
+
+
+  return (
+    <div className='min-h-screen bg-gradient-to-br from-gray-900 to-blue-900 text-white py-12'>
+      <div className='container mx-auto px-4'>
+        <BookInformation title={title} author={author} published_date={published_date} rating={rating} page_count={page_count}/>
+        <BookReviews reviews={reviews}/>
+      </div>
+    </div>
+  );
+
+  function isSameUser(review, user) {
+    return user.username === review.username;
+  }
 };
