0f67e079fd6c7c3de345e2d6b88cd462b8ca5ac7
matulibe <57047770+matulibe@users.noreply.github.com>
2024-11-13 14:18:41 -0300

    cambio simbolos

--- src/reads/pages/ChatPage.jsx
+++ src/reads/pages/ChatPage.jsx
@@ -5,15 +5,17 @@ import { supabase } from '../../utils/supabase-client';
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import SendIcon from '@mui/icons-material/Send';
+import { SendHorizontal } from 'lucide-react';
 import { useParams } from 'react-router-dom';
 
 
 export const ChatPage = () => {
     const [messages, setMessages] = useState([]);
     const [newMessage, setNewMessage] = useState('');
+    const [users, setUsers] = useState([]);
     const { authState: { user } } = useContext(AuthContext);
     const { chatroomID } = useParams();
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchMessages = async () => {
@@ -33,9 +35,33 @@ export const ChatPage = () => {
                 })));
             }
         };
-
         fetchMessages();
-    }, [chatroomID, user.username]);
+    }, [chatroomID]);
+
+    // useEffect(() => {
+    //     const fectchUsers = async () => {
+    //         const usernames = [...new Set(messages.map(message => message.sender))];
+    //         const [username1, username2] = usernames;
+
+    //         const { data, error } = await supabase
+    //             .from('users')
+    //             .select('username, profile_picture')
+    //             .in('username', [username1, username2])
+
+    //         if (error) {
+    //             console.error("Error al obtener los usuarios:", error.message);
+    //         } else {
+    //             setUsers(data);
+    //         }
+    //         fectchUsers();
+    //     };
+    // }, [messages]);
+
+    // useEffect(() => {
+    //     if (messages.length > 0 && users.length > 0) {
+    //         setIsLoading(false);
+    //     }
+    // }, [messages, users]);
     
     const handleSendMessage = async () => {
         if (newMessage.trim() === '') return;
@@ -67,6 +93,8 @@ export const ChatPage = () => {
         }
     }
 
+    
+
     return (
         <div className='min-h-screen bg-gradient-to-br from-gray-900 to-blue-900 text-white py-12'>
             <div className='container mx-auto px-4'>
@@ -77,10 +105,9 @@ export const ChatPage = () => {
                             {messages.map((message, index) => (
                                 <div key={index} className={`flex ${message.sender === user.username ? 'justify-end' : 'justify-start'}`}>
                                     <div className='flex items-center space-x-3'>
-                                        {message.sender === 'partner' && (
+                                        {message.sender !== user.username && (
                                             <Avatar>
-                                                <AvatarImage src='/path/to/partner-avatar.jpg' alt='Partner' />
-                                                <AvatarFallback>Partner</AvatarFallback>
+                                                <AvatarImage src={users.find(user => user.username === message.sender)?.profile_picture || '/default-avatar.png'}/>
                                             </Avatar>
                                         )}
                                         <div className={`bg-${message.sender === user.username ? 'green-600' : 'blue-600'} rounded-lg px-4 py-2 shadow-md`}>
@@ -89,8 +116,7 @@ export const ChatPage = () => {
                                         </div>
                                         {message.sender === user.username && (
                                             <Avatar>
-                                                <AvatarImage src='/path/to/user-avatar.jpg' alt='You' />
-                                                <AvatarFallback>You</AvatarFallback>
+                                                <AvatarImage src={users.find(user => user.username === message.sender)?.profile_picture || '/default-avatar.png'} alt='You' />
                                             </Avatar>
                                         )}
                                     </div>
@@ -111,7 +137,7 @@ export const ChatPage = () => {
                                 onClick={handleSendMessage}
                                 className='bg-blue-500 hover:bg-blue-600 text-white w-10 h-10 rounded-full flex items-center justify-center'
                             >
-                                <SendIcon />
+                                <SendHorizontal />
                             </Button>
                         </div>
                     </CardContent>
